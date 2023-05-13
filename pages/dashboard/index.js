d6d83dd7c3ed8f22c21f43f2710d30ec5f9d0e70
James Diecidue <jamesdiecidue@gmail.com>
2023-05-13 18:43:40 -0400

    ticket sorting by status and updatedAt

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import DashboardLayout from '../../components/DashboardLayout';
 import UserContext from '../../contexts/UserContext';
 import TicketService from '../../modules/TicketService';
+import DayJs from 'dayjs';
 
 export default function Tickets() {
 	const userContext = useContext(UserContext);
@@ -14,12 +15,12 @@ export default function Tickets() {
 			router.push('/dashboard/settings');
 		}
 		if (userContext.helpUser.tickets) {
-			if(userContext.helpUser.tickets.length < 1){
+			if (userContext.helpUser.tickets.length < 1) {
 				router.push('/dashboard/create');
 			} else {
 				setTickets(userContext.helpUser.tickets);
 			}
-		} else{
+		} else {
 			router.push('/dashboard/create');
 		}
 	}, [router, userContext.helpUser, userContext.helpUser.name, userContext.helpUser.tickets]);
@@ -36,22 +37,37 @@ export default function Tickets() {
 						}
 						{TicketService.isAdmin(userContext.helpUser) &&
 							<>All Tickets
-								 &nbsp;<span className="bg-gray-100 text-gray-800 text-xs tex uppercase font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">Admin</span>
+								&nbsp;<span className="bg-gray-100 text-gray-800 text-xs tex uppercase font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">Admin</span>
 
 							</>
 						}
 					</h1>
 					<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-						{tickets.map((ticket, index) => (
-							<div key={index} className="border border-gray-200 p-4 rounded-lg shadow-sm hover:cursor-pointer" onClick={() => router.push(`/dashboard/${ticket.id}`)}>
-								<div className="text-xl text-gray-600">
-									{ticket.description.length > 30
-										? ticket.description.substring(0, 30) + '...'
-										: ticket.description}
+						{tickets
+							.slice()
+							.sort((a, b) => {
+								// Sort by status (ascending)
+								if (a.status < b.status) return -1;
+								if (a.status > b.status) return 1;
+
+								// If status is equal, sort by updatedAt (descending)
+								if (a.updatedAt < b.updatedAt) return 1;
+								if (a.updatedAt > b.updatedAt) return -1;
+
+								// If both status and updatedAt are equal
+								return 0;
+							}).map((ticket, index) => (
+								<div key={index} className="border border-gray-200 p-4 rounded-lg shadow-sm hover:cursor-pointer" onClick={() => router.push(`/dashboard/${ticket.id}`)}>
+									<div className="text-xl text-gray-600">
+										{ticket.description.length > 30
+											? ticket.description.substring(0, 30) + '...'
+											: ticket.description}
+									</div>
+									<div className="text-sm font-semibold text-gray-700 mb-2">Status: {TicketService.getStatus(ticket.status)}</div>
+									<div className="text-sm font-semibold text-gray-700 mb-2">{DayJs(ticket.updatedAt).format('MM/DD/YYYY')}</div>
+
 								</div>
-								<div className="text-sm font-semibold text-gray-700 mb-2">Status: {TicketService.getStatus(ticket.status)}</div>
-							</div>
-						))}
+							))}
 
 					</div>
 					{tickets.length < 1 &&
