f9bffe4c9c5d1025ce3f1085ee19542057a42b7c
James Diecidue <jamesdiecidue@gmail.com>
2023-05-13 14:33:31 -0400

    message fixes

--- pages/dashboard/[id].js
+++ pages/dashboard/[id].js
@@ -29,7 +29,6 @@ export default function TicketMessages() {
                 if (response.ok) {
                     const data = await response.json();
                     setTicket(data.ticket);
-					console.log('data.ticket', data.ticket);
                 }
             }
         }
@@ -40,11 +39,11 @@ export default function TicketMessages() {
 		<DashboardLayout>
 			<div className="py-6">
 				<div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-					<h1 className="text-2xl font-semibold text-gray-900 mb-4">Ticket {ticket.id} </h1>
+					<h1 className="text-2xl font-semibold text-gray-900 mb-4">Ticket {ticket.id} - {ticket.description} </h1>
 					<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
 						{ticket.messages.map((message, index) => (
 							<div key={index} className="border border-gray-200 p-4 rounded-lg shadow-sm hover:cursor-pointer">
-								<p>{message}</p>
+								<p>{message.text}</p>
 							</div>
 						))}
 					</div>
