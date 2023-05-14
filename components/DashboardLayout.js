2cc989d40bc94a480c57372fbb95cf55f87573d1
James Diecidue <jamesdiecidue@gmail.com>
2023-05-13 20:19:29 -0400

    hover cursor pointer for user nav

--- components/DashboardLayout.js
+++ components/DashboardLayout.js
@@ -212,7 +212,7 @@ export default function DashboardLayout({ children }) {
 															href={item.href}
 															className={classNames(
 																active ? 'bg-gray-100' : '',
-																'block px-4 py-2 text-sm text-gray-700'
+																'block px-4 py-2 text-sm text-gray-700 hover:cursor-pointer'
 															)}
 														>
 															{item.name}
