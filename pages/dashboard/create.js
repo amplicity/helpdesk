f1cda891e932e81d678dccaa4b4605f5fd9e0b23
James Diecidue <jamesdiecidue@gmail.com>
2023-05-14 00:21:22 -0400

    revert form

--- pages/dashboard/create.js
+++ pages/dashboard/create.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import DashboardLayout from '../../components/DashboardLayout';
 import UserContext from '../../contexts/UserContext';
@@ -6,8 +6,6 @@ import UserContext from '../../contexts/UserContext';
 export default function Create() {
 	const router = useRouter();
 	const userContext = useContext(UserContext);
-	const [text, setText] = useState('');
-	const [description, setDescription] = useState('');
 
 	useEffect(() => {
 		if (!userContext.helpUser.name) {
@@ -15,8 +13,10 @@ export default function Create() {
 		}
 	}, [router, userContext.helpUser]);
 
-	const handleTicketOnSubmit = async (e) => {
-		e.preventDefault();
+	const handleTicketOnSubmit = async (event) => {
+		event.preventDefault();
+		const description = event.target.description.value;
+		const text = event.target.text.value;
 		const ticket = { description, text };
 		try {
 			const response = await fetch('/api/tickets/create', {
@@ -46,13 +46,6 @@ export default function Create() {
 			console.error('Error:', error);
 		}
 	};
-	const handleTextOnChange = (e) => {
-		setText(e.target.value);
-	}
-
-	const handleDescriptionOnChange = (e) => {
-		setDescription(e.target.value);
-	}
 
 	return (
 		<DashboardLayout>
@@ -62,7 +55,7 @@ export default function Create() {
 				</div>
 				<div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
 					<div className="flex flex-col ">
-						<form action="#">
+						<form onSubmit={handleTicketOnSubmit} action="#">
 							<div className="mt-4">
 								<label htmlFor="description" className="block text-sm font-medium text-gray-700">
 									Subject
@@ -71,7 +64,6 @@ export default function Create() {
 									type="text"
 									name="description"
 									id="description"
-									onChange={handleDescriptionOnChange}
 									className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
 								/>
 							</div>
@@ -83,11 +75,10 @@ export default function Create() {
 									name="text"
 									id="text"
 									rows="3"
-									onChange={handleTextOnChange}
 									className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
 								></textarea>
 							</div>
-							<button onClick={handleTicketOnSubmit} disabled={description == '' || text == '' || text == undefined || description == undefined}  className={`mt-4 z-50 text-white font-bold py-2 px-4 bg-slate-500 rounded hover:cursor-pointer`} >
+							<button name="submit" type="submit" className={` mt-4 z-90 text-white font-bold py-2 px-4 bg-slate-500 rounded hover:cursor-pointer`} >
 								Create Ticket
 							</button>
 
